655a65960d1eee9ba2b70ee9b90c4fa73d5a0ddf
Jukakombo <alisonmagid2016@gmail.com>
2020-05-13 23:18:01 +0200

    add all the files

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const encrypt= require("mongoose-encryption");
+
+app.set('views engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true});
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+app.get("/register", function(req, res){
+  res.render("register.ejs");
+});
+
+
+app.get("/login", function(req, res){
+  res.render("login.ejs");
+});
+
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"]});
+const User=mongoose.model("User", userSchema);
+
+app.post("/register", function(req, res){
+  const newUser= new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets.ejs");
+    }
+  });
+});
+
+
+ app.post("/login", function(req, res){
+   const username=req.body.username;
+   const password=req.body.password;
+   User.findOne({email:username}, function(err, foundUser){
+     if(err){
+       console.log(err);
+     }else{
+       if(foundUser){
+         if(foundUser.password === password){
+           res.render("secrets.ejs");
+         }
+       }
+     }
+   });
+ });
+
+app.listen(process.env.PORT || 3000, function(req, res){
+  console.log("your app is running on port 3000");
+});
